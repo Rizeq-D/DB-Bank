68ed53ce5bb165123e4000f46ee93995248cf4a7
Abdulrazak <rizeq@Abdulrazaks-MBP.fritz.box>
2023-10-22 02:45:28 +0200

    solved a problem regarding the ID

--- BankAcounts.java
+++ BankAcounts.java
@@ -1,25 +1,20 @@
 class BankAccounts {
     private double money = 0;
     private int id;
-
     public BankAccounts(int id) {
         this.id = id;
     }
-
+    public int getId() {
+        return id;
+    }
     public void add(double moneyToAdd) {
         money += moneyToAdd;
     }
-
     public void withdraw(double moneyToWithdraw) {
         money -= moneyToWithdraw;
     }
-
     public void printDetails() {
         System.out.println("ID: " + id + " Money: " + money + " Euro");
     }
 
-    public Integer getId() {
-
-        return id;
-    }
 }
